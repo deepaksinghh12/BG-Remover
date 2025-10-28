573eca4bdd3fa0351fc9da157dbcfbd516cb1f72
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-10-29 00:35:34 +0530

    Commit

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -11,14 +11,14 @@ const Testimonials = () => {
 
             {/* Testimonials content will go here */}
             {testimonialsData.map((item, index) => (
-                <div className='bg-white rounded-xl p-6 drop-shadow-md max-w-lg m-auto' key={index}>
-                    <p>"</p>
-                    <p>{item.text}</p>
-                    <div>
-                        <img src={item.image} alt="" />
+                <div className='bg-white rounded-xl p-6 drop-shadow-md max-w-lg m-auto hover:scale-105 transition-all duration-700' key={index}>
+                    <p className='text-4xl text-gray-500'>"</p>
+                    <p className='text-sm text-gray-500'>{item.text}</p>
+                    <div className='flex items-center gap-3 mt-5'>
+                        <img className='w-9 rounded-full' src={item.image} alt="" />
                         <div>
                             <p>{item.author}</p>
-                            <p>{item.jobTitle}</p>    
+                            <p className='text-sm text-gray-600'>{item.jobTitle}</p>    
                         </div>
                     </div>
                 </div>
